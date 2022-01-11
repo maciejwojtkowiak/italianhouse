36c1cce037362e555af49d9f05033331e6b47aa7
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-01-11 22:19:19 +0100

    first post request to firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,20 +10,27 @@ import { useSelector } from "react-redux"
 const Cart = () => {
 
     const cartItems = useSelector(state => state.cart.items)
-    const onOrderHandler = () => {
+    const onOrderHandler = (e) => {
+        e.preventDefault()
         fetch('https://italianhouse-1aef0-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
             method: 'POST',
-            body: JSON.stringify()
+            body: JSON.stringify(cartItems),
+            headers: {
+                'content-type': 'application/json'
+            }
         })
     }
 
     return ReactDOM.createPortal(
-        <Overlay>
+       
             <div className={styles.cart}> 
                 <CartItems />
-                <button className={styles.button}>Order</button>
-            </div>
-        </Overlay>,
+                <form onSubmit={onOrderHandler}>
+                    <button className={styles.button}>Order</button>
+                </form>
+                
+            </div>,
+        
         
 
        
