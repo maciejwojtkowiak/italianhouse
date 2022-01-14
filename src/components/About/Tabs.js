ab23dad6b5700b369b760df3196e796811d1654e
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-01-14 13:06:14 +0100

    created tabs and dynamic content

--- src/components/About/Tabs.js
+++ src/components/About/Tabs.js
@@ -1,17 +1,41 @@
 import styles from './Tabs.module.css'
 import Tab from './Tab'
 import { useState } from 'react'
+import Content from './Content'
 
 const Tabs = () => {
     const [activeTab, setActiveTab] = useState(1)
+    const firstTabContent = (
+    <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    </p> )
+
+    const secondTabContent = (
+    <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    </p> )
+
+    const thirdTabContent = (
+    <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    </p> )
 
    
     return (
         <div className={styles.tabs}>
-            {activeTab === 1 && <Tab name='first' /> }
+            <Tab name='first' /> 
             <Tab />
             <Tab />
-
+            {activeTab === 1 && <Content header="Secret recipe" content= {firstTabContent} />}
             
         </div>
     )
