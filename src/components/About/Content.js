111aec062e4159da47a9ae2de762c4974fc547ba
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-01-20 21:00:03 +0100

    transition for tabbed component

--- src/components/About/Content.js
+++ src/components/About/Content.js
@@ -1,12 +1,22 @@
+import React from 'react'
 import styles from './Content.module.css'
+import { CSSTransition } from 'react-transition-group'
+import './Content.css'
 
 const Content = (props) => {
-
+    const isActive = (props.isActive)
     return (
-        <div className={styles.tabContent}>
-            <h1>{props.header}</h1>
-            <p>{props.content}</p>
-        </div>
+        <React.Fragment>
+            <CSSTransition unmountOnExit in={isActive} timeout={1000} classNames='content'>
+                <div className={styles.tabContent}>
+                    <h1>{props.header}</h1>
+                    <p>{props.content}</p>
+                </div>
+            </CSSTransition>
+           
+        </React.Fragment>
+        
+        
     )
 }
 
