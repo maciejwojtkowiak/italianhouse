c8691c81d66e8d10a315ca97fc1255f3a4ff374a
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-01-23 16:19:33 +0100

    positioned hero section thanks to grid

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,22 +17,24 @@ const Hero = (props) => {
         <div className={styles.heroContainer}>
             <Header scrollToMeals={smoothScrollHandlerMeals} scrollToAbout={smoothScrollHandlerAbout} />
             <div className={styles.description}>
-                <div className={styles.description__leftside}>
-                <h3 className={styles.description__header}>REAL ITALIAN PIZZA</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Aliquam nisi sapien, laoreet et tellus sed, dictum dignissim 
-                    lectus. Nulla a nulla tortor. Aliquam et risus nec ante pulvinar 
-                    bibendum. Nam eget tellus diam. Phasellus mauris lectus, blandit 
-                    at blandit tempor, lacinia at tellus. Maecenas rutrum aliquet 
-                    eros quis faucibus. Fusce eu nibh in massa pharetra sollicitudin 
-                    ac ornare lacus. Nulla condimentum ac nulla at aliquet. Integer 
-                    quis tincidunt lorem. Nulla rutrum ullamcorper lobortis.</p>
-
-                <button className={styles.orderButton} onClick={smoothScrollHandlerMeals}>Order now</button>
-            </div>
+                <div className={styles['left-side-description']}>
+                    <h3 className={styles.description__header}>REAL ITALIAN PIZZA</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Aliquam nisi sapien, laoreet et tellus sed, dictum dignissim 
+                        lectus. Nulla a nulla tortor. Aliquam et risus nec ante pulvinar 
+                        bibendum. Nam eget tellus diam. Phasellus mauris lectus, blandit 
+                        at blandit tempor, lacinia at tellus. Maecenas rutrum aliquet 
+                        eros quis faucibus. Fusce eu nibh in massa pharetra sollicitudin 
+                        ac ornare lacus. Nulla condimentum ac nulla at aliquet. Integer 
+                        quis tincidunt lorem. Nulla rutrum ullamcorper lobortis.
+                    </p>
+                    <button className={styles.orderButton} onClick={smoothScrollHandlerMeals}>Order now</button>
+                </div>
                 <div className={styles.description__rightside}>
                     <img alt="pizza img" className={styles.heroImage} src={pizza} />
                 </div>
+                
+                
                 
             </div>
         </div>
