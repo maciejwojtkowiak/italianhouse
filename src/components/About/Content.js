8f8dff91d391308c04b56ea276a12312040ac7b1
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-01-24 19:51:37 +0100

    fixed restaurantName

--- src/components/About/Content.js
+++ src/components/About/Content.js
@@ -7,7 +7,7 @@ const Content = (props) => {
     const isActive = (props.isActive)
     return (
         <React.Fragment>
-            <CSSTransition in={isActive} timeout={1000} classNames='content'>
+            <CSSTransition mountOnEnter in={isActive} timeout={1000} classNames='content'>
             
                 <div className={styles.tabContent}>
                     <h1>{props.header}</h1>
